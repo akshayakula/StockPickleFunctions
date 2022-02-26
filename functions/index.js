01c2b899b552766707f779feb174a5c843a124e8
akshayakula <akshayakula@vt.edu>
2022-02-26 13:50:50 -0500

    keys.json

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,12 @@
 const functions = require("firebase-functions");
-
+let keys = require('../keys.json')
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   
     functions.logger.info("Hello logs!", {structuredData: true});
+    console.log(keys)
     response.send("Hello from Firebase!");
 
 });
